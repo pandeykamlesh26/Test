8ff0b28dce61ca0931baab8c58d799ae51510606
pandeykamlesh26 <kamalcricketindia@gmail.com>
2024-09-10 17:18:05 +0530

    second commit

--- friendship/apps.py
+++ friendship/apps.py
@@ -3,5 +3,4 @@ from django.apps import AppConfig
 
 class FriendshipConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "Mobile-Django-Boilerplate-2.0-Friends-Module"
-    label = "friendship"
+    name = "friendship"
